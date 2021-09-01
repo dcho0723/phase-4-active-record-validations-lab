f3d89881b6382e3a7a051336e23132d4d288edb2
dcho0723 <dcho0723@yahoo.com>
2021-08-31 21:45:48 -0400

    finished

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :title_validation
+
+
+
+    def title_validation
+        unless title.present? && title.match?("Won't Believe" || "Secret" || "Top [number]" || "Guess")
+            errors.add(:title, "some weird click bait here though")
+        end
+    end
+
 end
